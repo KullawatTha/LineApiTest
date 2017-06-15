285bd081f6d92ab00edfcdd0f44370f641795f65
KullawatTha <tnamekung@gmail.com>
2017-06-15 14:35:58 +0700

    Create index.php

--- index.php
+++ index.php
@@ -52,7 +52,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $title,$text
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
