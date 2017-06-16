b7c3f54a70ce18bb35a5ceccd73761dfea2dde22
KullawatTha <tnamekung@gmail.com>
2017-06-16 09:55:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			// Build message to reply back
-			$messages = [
+			$messages[2] = [
 				{'type' => 'text',
 				'text' => $text},
 				{'type' => 'sticker',
