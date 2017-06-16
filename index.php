2e00e5cf6b8f094b1780638288e4370c5f153c68
KullawatTha <tnamekung@gmail.com>
2017-06-16 09:50:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,19 +10,17 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'location') {
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['message']['address'];
-            $la = $event['message']['latitude'];
+			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
-
 			// Build message to reply back
 			$messages = [
-                	'type' => 'location',
-                	'address' => $text,
-                	'latitude' => $la
-
+                		
+                		'type' => 'sticker',
+	             		'packageId' => '1',
+               			'stickerId' => '20'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
